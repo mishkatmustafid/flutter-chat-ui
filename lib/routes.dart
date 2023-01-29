dcae75e41a099ea1d9efa50f39d3123969568c68
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-29 18:24:20 +0600

    routes - lint

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,5 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_chat_ui/homepage.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  '/': (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+  '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
 };

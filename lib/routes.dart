ab2558a4c652d315dc0fe81b6f7b9a915acbf65c
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-29 15:15:41 +0600

    starter code added

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_chat_ui/homepage.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  '/': (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+};
